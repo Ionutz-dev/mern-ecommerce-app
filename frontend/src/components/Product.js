c4ef7d0ea3aa8f923a2c6b67cd33c2e6811fca3e
Ionutz-dev <rasalghul4023@gmail.com>
2021-12-21 21:30:37 +0200

    upgraded react_router

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import { Card } from 'react-bootstrap';
 import Rating from './Rating';
@@ -9,15 +10,15 @@ const Product = props => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Card.Body>
-        <a to={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Img src={product.image} variant='top' />
-        </a>
+        </Link>
 
-        <a to={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
 
         <Card.Text as='div'>
           <Rating
